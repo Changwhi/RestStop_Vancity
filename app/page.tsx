6e7b00fcf76119cc21b727234ee6a69662feda88
Changwhi <whinada@gmail.com>
2024-04-10 22:35:28 -0700

    feat: import fetching data function and test it

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import  { getVancouverParkWashroomData, getVancouverPublicWashroomData} from "@/app/lib/washrooms/vancouver";
 
-export default function Home() {
+export default async function Home() {
+  const data = await getVancouverParkWashroomData();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    "Welcome2"
+      {JSON.stringify(data)}
     </main>
   );
 }
