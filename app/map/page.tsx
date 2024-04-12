cbf4051794e09e52df419192157481f47cd96f7e
Changwhi <whinada@gmail.com>
2024-04-11 17:05:38 -0700

    feat: implement new route for map

---
+++ app/map/page.tsx
@@ -0,0 +1,10 @@
+import MapForWashrooms from "@/components/map/MapForWashrooms";
+
+export default function MapPage() {
+  return (
+    <div>
+      <h1>Map Page</h1>
+      <MapForWashrooms />
+    </div>
+  );
+}
