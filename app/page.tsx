226bdae49f3cdb584dcc2359ea710a175871e372
Changwhi <whinada@gmail.com>
2024-04-15 10:53:38 -0700

    fix: change " to &quot; to avoid error during running build script

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>"Hello, World!"</p>
+      <p>&quot;Hello, World!&quot;</p>
     </main>
   );
 }
