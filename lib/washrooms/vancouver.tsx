7770ccb70285eefbda9fd663ef55c72f0a205f78
Changwhi <whinada@gmail.com>
2024-04-15 10:24:20 -0700

    fix: modify type

--- lib/washrooms/vancouver.tsx
+++ lib/washrooms/vancouver.tsx
@@ -1,32 +1,33 @@
 // For public washrooms in vancouver
-interface WashroomData {
+interface PublicWashroomApiResponse {
   total_count: number;
-  results: [
-    {
-      name: string;
-      address: string;
-      type: string;
-      location: string;
-      summer_hours: string;
-      winter_hours: string;
-      wheel_access: string;
-      maintainer: string;
-      note: string;
-      geom: {
-        type: string;
-        coordinates: number[];
-      };
-      geo_local_area: string;
-      primaryind: string;
-      geo_point_2d: {
-        type: string;
-        coordinates: number[];
-      };
-    }
-  ];
+  results: PublicWashroomData[];
 }
-
-export async function getVancouverPublicWashroomData(): Promise<WashroomData> {
+interface PublicWashroomData {
+     name: string;
+  address: string;
+  type: string;
+  location: string;
+  summer_hours: string;
+  winter_hours: string;
+  wheel_access: string;
+  maintainer: string;
+  note: string;
+  geom: {
+    type: string;
+    coordinates: number[];
+  };
+  geo_local_area: string;
+  primaryind: string;
+  geo_point_2d: {
+    type: string;
+    coordinates: number[];
+    lat: number; 
+    lon: number; 
+  };
+}
+export async function getVancouverPublicWashroomData(): Promise<PublicWashroomData[]>{
+  try {
   const response = await fetch(
     "https://opendata.vancouver.ca/api/explore/v2.1/catalog/datasets/public-washrooms/records?limit=20",
     {
@@ -40,9 +41,11 @@ export async function getVancouverPublicWashroomData(): Promise<WashroomData> {
     throw new Error("Failed to fetch data" + response.statusText);
   }
 
-  const responseData: WashroomData = await response.json();
-  console.log(responseData.results[0]);
-  return responseData;
+  const responseData: PublicWashroomApiResponse = await response.json();
+  return responseData.results;
+  } catch (error) {
+   throw new Error("getVancouverPublicWashroomData: " + error); 
+  }
 }
 
 // For washroom at parks in vancouver
@@ -61,6 +64,7 @@ interface ParkApiResponse {
 }
 
 export async function getVancouverParkWashroomData(): Promise<ParkApiResponse> {
+  try {
   const response = await fetch(
     "https://opendata.vancouver.ca/api/explore/v2.1/catalog/datasets/parks-washrooms/records?limit=94",
     {
@@ -77,4 +81,7 @@ export async function getVancouverParkWashroomData(): Promise<ParkApiResponse> {
   const responseData: ParkApiResponse = await response.json();
   console.log(responseData.results);
   return responseData;
+  } catch (error:any) {
+      throw new Error("getVancouverParkWashroomData: " + error.message);
+  }
 }
