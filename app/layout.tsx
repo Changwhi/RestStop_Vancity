562e543bf1d6b26b088b2407ccb5dac9a325410f
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-04 23:29:29 -0700

    Import Navbar and config from tailwind css to prevent autoAddCss and apply styling to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
+config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex flex-col h-screen max-h-screen">
+          <Navbar />
+          {/* background styling goes in this div*/}
+          <div className="flex-grow overflow-y-auto bg-page text-default-text">
+            {children}
+          </div>
+        </div>
+      </body>{" "}
     </html>
   );
 }
