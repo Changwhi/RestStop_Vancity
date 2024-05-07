4e0626a6d3b4d4a220e636ea27e5e5e485d81fe4
briankjkim <97319869+briankjkim@users.noreply.github.com>
2024-05-07 15:52:56 -0700

    Change Navbar to client component to use useState, place mobile menu icon, rearrange icons in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import {
   faAddressCard,
+  faBars,
   faBookmark,
   faCircleQuestion,
   faHome,
@@ -8,57 +11,73 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleToggle = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
   return (
     <nav className="flex justify-between bg-nav p-4">
-      <div className="flex items-center space-x-4">
-        <span className=" text-default-text text-base md:text-xl">RestStop Vancity 🚻</span>
-        <Link href={"/"}>
-          {/* HOME */}
-          <div className="flex flex-col">
-            <FontAwesomeIcon icon={faHome} className="icon mb-2" />
-            Home
+      <span className=" text-default-text text-base sm:text-xl">
+        RestStop Vancity 🚻
+      </span>
+      <div id="desktop-menu" className="hidden sm:flex justify-between">
+        <div className="flex items-center space-x-4">
+          <Link href={"/"}>
+            {/* HOME */}
+            <div className="flex flex-col">
+              <FontAwesomeIcon icon={faHome} className="icon mb-2" />
+              Home
+            </div>
+          </Link>
+          <Link href={"/"}>
+            {/* Find Bathroom */}
+            <div className="flex flex-col">
+              <FontAwesomeIcon icon={faRestroom} className="icon mb-2" />
+              Bathroom
+            </div>
+          </Link>
+          <Link href={"/"}>
+            {/* About */}
+            <div className="flex flex-col">
+              <FontAwesomeIcon icon={faCircleQuestion} className="icon mb-2" />
+              About
+            </div>
+          </Link>
+          <Link href={"/"}>
+            {/* Favorites */}
+            <div className="flex flex-col">
+              <FontAwesomeIcon icon={faBookmark} className="icon mb-2" />
+              Favorites
+            </div>
+          </Link>
+          <div className="flex items-center space-x-4">
+            {/* Log In  */}
+            <Link href={"/LogIn"}>
+              <div className="flex flex-col">
+                <FontAwesomeIcon
+                  icon={faRightFromBracket}
+                  className="icon mb-2"
+                />
+                LogIn
+              </div>
+            </Link>
+            <Link href={"/Profile"}>
+              {/* Profile */}
+              <div className="flex flex-col text-default-text text-xs">
+                <FontAwesomeIcon icon={faAddressCard} className="icon mb-2" />
+                Profile
+              </div>
+            </Link>
           </div>
-        </Link>
-        <Link href={"/"}>
-          {/* Find Bathroom */}
-          <div className="flex flex-col">
-            <FontAwesomeIcon icon={faRestroom} className="icon mb-2" />
-            Bathroom
-          </div>
-        </Link>
-        <Link href={"/"}>
-          {/* About */}
-          <div className="flex flex-col">
-            <FontAwesomeIcon icon={faCircleQuestion} className="icon mb-2" />
-            About
-          </div>
-        </Link>
-        <Link href={"/"}>
-          {/* Favorites */}
-          <div className="flex flex-col">
-            <FontAwesomeIcon icon={faBookmark} className="icon mb-2" />
-            Favorites
-          </div>
-        </Link>
+        </div>
       </div>
-      <div className="flex items-center space-x-4">
-        {/* Log In  */}
-        <Link href={"/LogIn"}>
-          <div className="flex flex-col">
-            <FontAwesomeIcon icon={faRightFromBracket} className="icon mb-2" />
-            LogIn
-          </div>
-        </Link>
-        <Link href={"/Profile"}>
-          {/* Profile */}
-          <div className="flex flex-col text-default-text text-xs">
-            <FontAwesomeIcon icon={faAddressCard} className="icon mb-2" />
-            Profile
-          </div>
-        </Link>
+      <div id="mobile-menu" className="sm:hidden">
+        <FontAwesomeIcon icon={faBars} className="icon mb-2"/>
       </div>
     </nav>
   );
