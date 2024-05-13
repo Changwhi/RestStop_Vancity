44de462c526a47f492a6cf9709abb8a38ed12d29
Changwhi <whinada@gmail.com>
2024-05-13 03:20:57 -0700

    style: remove white spaces

--- components/map.tsx
+++ components/map.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Loader } from "@googlemaps/js-api-loader";
 import React, { useEffect, useState } from "react";
 import { getClosestWashrooms } from "@/lib/washrooms/getClosestWashrooms";
@@ -7,8 +6,6 @@ import SearchButton from "./searchButton";
 import getCurrentLocation from "@/lib/getCurrentLocation";
 import SearchResult from "./searchResult";
 import { PublicWashroomData, MapProps } from "@/types/washroom";
-
-
 
 export default function Map({ washrooms }: MapProps) {
   const [userLocation, setUserLocation] = useState({ lat: 0, lng: 0 });
@@ -61,16 +58,12 @@ export default function Map({ washrooms }: MapProps) {
         content: "",
         disableAutoPan: true,
       });
-
       // set up the map
       const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
-
       // put up a marker
       washrooms.forEach((washroom: PublicWashroomData) => {
         // set up icon
-
         const parser = new DOMParser();
-
         // A marker with a custom inline SVG.
         const pinSvgString =
           '<svg xmlns="http://www.w3.org/2000/svg" fill="blue" width="20" viewBox="0 0 640 512"><path d="M80 48a48 48 0 1 1 96 0A48 48 0 1 1 80 48zm40 304V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V325.2c-8.1 9.2-21.1 13.2-33.5 9.4c-16.9-5.3-26.3-23.2-21-40.1l30.9-99.1C44.9 155.3 82 128 124 128h8c42 0 79.1 27.3 91.6 67.4l30.9 99.1c5.3 16.9-4.1 34.8-21 40.1c-12.4 3.9-25.4-.2-33.5-9.4V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V352H120zM320 0c13.3 0 24 10.7 24 24V488c0 13.3-10.7 24-24 24s-24-10.7-24-24V24c0-13.3 10.7-24 24-24zM464 48a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zM440 480V384H422.2c-10.9 0-18.6-10.7-15.2-21.1l9-26.9c-3.2 0-6.4-.5-9.5-1.5c-16.9-5.3-26.3-23.2-21-40.1l29.7-95.2C428.4 156.9 467.6 128 512 128s83.6 28.9 96.8 71.2l29.7 95.2c5.3 16.9-4.1 34.8-21 40.1c-3.2 1-6.4 1.5-9.5 1.5l9 26.9c3.5 10.4-4.3 21.1-15.2 21.1H584v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V384H504v96c0 17.7-14.3 32-32 32s-32-14.3-32-32z"></path></svg>';
@@ -108,7 +101,6 @@ export default function Map({ washrooms }: MapProps) {
     <>
       <div style={{ height: "300px" }} ref={mapRef}></div>
       {<SearchButton onClick={searchButtonHandler} />}
-      {/* Conditionally render the result page with buttonClicked ternary operation */}
       {buttonClicked && (
         <SearchResult washrooms={closestWashrooms} />
       )}
