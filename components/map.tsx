ea4417ec08ad43f8b98d2b0748e77a7588c77f3f
Changwhi <whinada@gmail.com>
2024-05-13 03:08:10 -0700

    refactor: decouple a a component that displays a result

--- components/map.tsx
+++ components/map.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import {
-  faCircleCheck,
-  faRestroom,
-} from "@fortawesome/free-solid-svg-icons";
 import { Loader } from "@googlemaps/js-api-loader";
 import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import NavigateButton from "./navigateButton";
 import { getClosestWashrooms } from "@/lib/washrooms/getClosestWashrooms";
 import SearchButton from "./searchButton";
 import getCurrentLocation from "@/lib/getCurrentLocation";
+import SearchResult from "./searchResult";
 
 interface PublicWashroomData {
   name: string;
@@ -135,73 +130,13 @@ export default function Map({ washrooms }: MapProps) {
     initMap();
   }, [washrooms, userLocation]);
 
-  interface BathroomCardProps {
-    bathroom: {
-      name: string;
-      address: string;
-      status: boolean;
-      lat: number;
-      lon: number;
-    };
-  }
-
-  const BathroomCard: React.FC<{
-    key: string;
-    washroom: PublicWashroomData;
-  }> = ({washroom}) => {
-    return (
-      <div className="flex justify-between rounded-lg space-x-4 border-2 p-2 m-2 border-white">
-        <FontAwesomeIcon
-          icon={faRestroom}
-          className="icon m-3"
-          transform="grow-7"
-        />
-        <div className="flex flex-col">
-          <h3>{washroom.name}</h3>
-          <p className="text-xs">{washroom.address}</p>
-        </div>
-        <div className="flex justify-end">
-          <NavigateButton
-            lat={washroom.geo_point_2d.lat}
-            lon={washroom.geo_point_2d.lon}
-          ></NavigateButton>
-          <FontAwesomeIcon
-            icon={faCircleCheck}
-            style={{ color: "#0dc700" }}
-            className="icon m-4" //Location of status icon
-            transform="grow-11"
-          />
-        </div>
-      </div>
-    );
-  };
-
   return (
     <>
       <div style={{ height: "300px" }} ref={mapRef}></div>
       {<SearchButton onClick={searchButtonHandler} />}
       {/* Conditionally render the result page with buttonClicked ternary operation */}
       {buttonClicked && (
-        <>
-          <div className="text-white" id="result">
-            <span className="flex justify-center">
-              <h3>Result</h3>
-            </span>
-            {/* TODO: Populate the list with nearby bathrooms */}
-            <div id="description-bar" className="flex justify-around">
-              <span id="icon" className="mr-12"></span>
-              {/* description bar spacing is here */}
-              <span id="bathroom-title">Washroom</span>
-              <span id="navigate">Navigate</span>
-              <span id="status">Status</span>
-            </div>
-            <div id="searchResult" className="flex flex-col">
-              {closestWashrooms.map((bathroom) => (
-                <BathroomCard key={bathroom.primaryind} washroom={bathroom} />
-              ))}
-            </div>{" "}
-          </div>
-        </>
+        <SearchResult closestWashrooms={closestWashrooms} />
       )}
     </>
   );
