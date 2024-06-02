aae1378f2ca441c27847a235062e0c48f5e38436
Changwhi <whinada@gmail.com>
2024-06-01 23:28:33 -0700

    feat: implement function to scroll to the search result section

--- components/map.tsx
+++ components/map.tsx
@@ -16,6 +16,12 @@ export default function Map({ washrooms }: MapProps) {
   const mapRef = React.useRef<HTMLDivElement>(null);
 
   const searchButtonHandler = async () => {
+    // Scroll to the search result section
+    const currentSection = document.getElementById("result");
+    if (currentSection) {
+      currentSection.scrollIntoView({ behavior: "smooth" });
+    }
+    // Get the user's current location
     try {
       const currentLocation = await getCurrentLocation();
       setUserLocation({ lat: currentLocation.lat, lng: currentLocation.lng });
@@ -66,7 +72,7 @@ export default function Map({ washrooms }: MapProps) {
         const parser = new DOMParser();
         // A marker with a custom inline SVG.
         const pinSvgString =
-          '<svg xmlns="http://www.w3.org/2000/svg" fill="blue" width="20" viewBox="0 0 640 512"><path d="M80 48a48 48 0 1 1 96 0A48 48 0 1 1 80 48zm40 304V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V325.2c-8.1 9.2-21.1 13.2-33.5 9.4c-16.9-5.3-26.3-23.2-21-40.1l30.9-99.1C44.9 155.3 82 128 124 128h8c42 0 79.1 27.3 91.6 67.4l30.9 99.1c5.3 16.9-4.1 34.8-21 40.1c-12.4 3.9-25.4-.2-33.5-9.4V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V352H120zM320 0c13.3 0 24 10.7 24 24V488c0 13.3-10.7 24-24 24s-24-10.7-24-24V24c0-13.3 10.7-24 24-24zM464 48a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zM440 480V384H422.2c-10.9 0-18.6-10.7-15.2-21.1l9-26.9c-3.2 0-6.4-.5-9.5-1.5c-16.9-5.3-26.3-23.2-21-40.1l29.7-95.2C428.4 156.9 467.6 128 512 128s83.6 28.9 96.8 71.2l29.7 95.2c5.3 16.9-4.1 34.8-21 40.1c-3.2 1-6.4 1.5-9.5 1.5l9 26.9c3.5 10.4-4.3 21.1-15.2 21.1H584v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V384H504v96c0 17.7-14.3 32-32 32s-32-14.3-32-32z"></path></svg>';
+          '<svg xmlns="http://www.w3.org/2000/svg" fill="#0077D8" width="25" height="25" viewBox="0 0 640 512"><path d="M80 48a48 48 0 1 1 96 0A48 48 0 1 1 80 48zm40 304V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V325.2c-8.1 9.2-21.1 13.2-33.5 9.4c-16.9-5.3-26.3-23.2-21-40.1l30.9-99.1C44.9 155.3 82 128 124 128h8c42 0 79.1 27.3 91.6 67.4l30.9 99.1c5.3 16.9-4.1 34.8-21 40.1c-12.4 3.9-25.4-.2-33.5-9.4V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V352H120zM320 0c13.3 0 24 10.7 24 24V488c0 13.3-10.7 24-24 24s-24-10.7-24-24V24c0-13.3 10.7-24 24-24zM464 48a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zM440 480V384H422.2c-10.9 0-18.6-10.7-15.2-21.1l9-26.9c-3.2 0-6.4-.5-9.5-1.5c-16.9-5.3-26.3-23.2-21-40.1l29.7-95.2C428.4 156.9 467.6 128 512 128s83.6 28.9 96.8 71.2l29.7 95.2c5.3 16.9-4.1 34.8-21 40.1c-3.2 1-6.4 1.5-9.5 1.5l9 26.9c3.5 10.4-4.3 21.1-15.2 21.1H584v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V384H504v96c0 17.7-14.3 32-32 32s-32-14.3-32-32z"></path></svg>';
         const pinSvg = parser.parseFromString(
           pinSvgString,
           "image/svg+xml"
@@ -100,10 +106,10 @@ export default function Map({ washrooms }: MapProps) {
   return (
     <>
       <div style={{ height: "600px" }} ref={mapRef}></div>
-      {<SearchButton onClick={searchButtonHandler} />}
-      {buttonClicked && (
-        <SearchResult washrooms={closestWashrooms} />
-      )}
+      <section id="result">
+        {<SearchButton onClick={searchButtonHandler} />}
+        {buttonClicked && <SearchResult washrooms={closestWashrooms} />}
+      </section>
     </>
   );
 }
