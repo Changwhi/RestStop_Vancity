58908c8096f18dd029b0922400fdd1585454a621
Changwhi <whinada@gmail.com>
2024-06-02 14:07:17 -0700

    refactor: import type interfaces instead of defining types in this file

--- lib/util/getClosestWashrooms.tsx
+++ lib/util/getClosestWashrooms.tsx
@@ -1,30 +1,5 @@
-interface Coordinates {
-  lat: number;
-  lon: number;
-}
-interface PublicWashroomData {
-  name: string;
-  address: string;
-  type: string;
-  location: string;
-  summer_hours: string;
-  winter_hours: string;
-  wheel_access: string;
-  maintainer: string;
-  note: string;
-  geom: {
-    type: string;
-    coordinates: number[];
-  };
-  geo_local_area: string;
-  primaryind: string;
-  geo_point_2d: {
-    type: string;
-    coordinates: number[];
-    lat: number;
-    lon: number;
-  };
-}
+import { PublicWashroomData } from "@/types/washroom";
+import { Coordinates } from "@/types/coordinate";
 
 function calculateDistance(coord1: Coordinates, coord2: Coordinates): number {
   const R = 6371; // Radius of the earth in km
