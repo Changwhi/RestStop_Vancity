78bb72b63c11876112e4f4951b99c1f6172e49c0
Changwhi <whinada@gmail.com>
2024-06-01 21:32:35 -0700

    refactor: create constant variable for text

--- components/searchButton.tsx
+++ components/searchButton.tsx
@@ -5,13 +5,14 @@ type SearchButtonProps = {
 };
 
 const SearchButton: React.FC<SearchButtonProps> = ({ onClick }) => {
+  const BUTTON_TEXT = "Find washrooms";
   return (
     <span className="flex justify-center">
       <button
         onClick={onClick}
         className="bg-blue-500 hover:bg-blue-700 mt-4 text-white font-bold py-2 px-4 rounded"
       >
-        Click
+      {BUTTON_TEXT}
       </button>
     </span>
   );
