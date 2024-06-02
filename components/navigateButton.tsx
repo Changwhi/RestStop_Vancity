997fc0354890c798d567591c1f5678f3ae8419fb
Changwhi <whinada@gmail.com>
2024-06-02 04:20:14 -0700

    rename: move openGoogleMapApp file to another folder

--- components/navigateButton.tsx
+++ components/navigateButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-import openGoogleMapApp from "@/lib/openGoogleMapApp";
+import openGoogleMapApp from "@/lib/util/openGoogleMapApp";
 import NavigateButtonIcon from "./icon/navigateButton";
 import { Coordinates } from "@/types/coordinate";
 
