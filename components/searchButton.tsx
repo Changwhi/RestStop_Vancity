457c0653a9abc022099eba6d2e87db308a6947a1
Changwhi <whinada@gmail.com>
2024-06-06 14:03:00 -0700

    design: change color from blue to black for consistency

--- components/searchButton.tsx
+++ components/searchButton.tsx
@@ -10,7 +10,7 @@ const SearchButton: React.FC<SearchButtonProps> = ({ onClick }) => {
     <span className="flex justify-center">
       <button
         onClick={onClick}
-        className="bg-blue-500 hover:bg-blue-700 mt-4 text-white font-bold py-2 px-4 rounded"
+        className="bg-black dark:bg-white hover:bg-blue-700 mt-4 text-white dark:text-black font-bold py-2 px-4 rounded"
       >
       {BUTTON_TEXT}
       </button>
