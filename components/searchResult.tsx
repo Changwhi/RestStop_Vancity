b6baf28022cdc8ca56d249c1f454aa790569dce8
Changwhi <whinada@gmail.com>
2024-06-09 01:51:41 -0700

    fix: replace key props since primaryind value is not unique value

--- components/searchResult.tsx
+++ components/searchResult.tsx
@@ -4,12 +4,13 @@ import { faCircleCheck, faRestroom } from "@fortawesome/free-solid-svg-icons";
 import { PublicWashroomData, MapProps } from "@/types/washroom";
 
 const BathroomCard: React.FC<{
-  key: string;
   washroom: PublicWashroomData;
 }> = ({ washroom }) => {
   const UNIT = "km";
   return (
-    <section className="flex justify-between w-5/6 md:w-3/5 rounded-lg space-x-4 border-2 p-2 m-2 border-black dark:border-white">
+    <section
+      className="flex justify-between w-5/6 md:w-3/5 rounded-lg space-x-4 border-2 p-2 m-2 border-black dark:border-white"
+    >
       <FontAwesomeIcon
         icon={faRestroom}
         className="icon m-3"
@@ -17,7 +18,10 @@ const BathroomCard: React.FC<{
       />
       <section className="flex-col items-start w-full">
         <p className="text-lg">{washroom.name}</p>
-        <p className="text-xs">{washroom.distance.toFixed(2)}{UNIT}</p>
+        <p className="text-xs">
+          {washroom.distance.toFixed(2)}
+          {UNIT}
+        </p>
       </section>
       <section className="flex justify-end">
         <NavigateButton
@@ -33,10 +37,12 @@ const SearchResult: React.FC<MapProps> = ({ washrooms }) => {
   return (
     <>
       <section className="flex-col">
-        <h3 className="text-2xl font-bold text-center p-5" >Most closest washrooms</h3>
+        <h3 className="text-2xl font-bold text-center p-5">
+          Most closest washrooms
+        </h3>
         <div id="searchResult" className="flex flex-col items-center">
-          {washrooms.map((bathroom) => (
-            <BathroomCard key={bathroom.primaryind} washroom={bathroom} />
+          {washrooms.map((bathroom, index) => (
+            <BathroomCard key={index} washroom={bathroom} />
           ))}
         </div>{" "}
       </section>
