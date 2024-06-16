c4aab426e849ad34e9a9762ed64906ce57529e14
Changwhi <whinada@gmail.com>
2024-06-16 02:39:20 -0700

    desin: update theme for UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+       keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
       },
     },
   },
