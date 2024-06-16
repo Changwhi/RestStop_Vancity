66687b89928f3263aa1328c719a117feeba0ccbc
Changwhi <whinada@gmail.com>
2024-06-16 02:44:39 -0700

    design: increase width for better UX

--- components/UI/card.tsx
+++ components/UI/card.tsx
@@ -3,7 +3,7 @@ import { ChildrenProps } from "@/types/ui"
 export default function Card(props: ChildrenProps) {
     return (
         <>
-            <li className="w-full sm:w-5/6 flex flex-col border border-solid border-slate-900 dark:border-gray-100 bg-white dark:bg-black py-6 px-2 rounded-3xl shadow-xl">
+            <li className="w-full sm:w-4xl flex flex-col border border-solid border-slate-200 dark:border-gray-700 bg-gray-100 dark:bg-gray-700 py-6 px-2 rounded-3xl shadow-xl">
             {props.children}
             </li>
         </>
